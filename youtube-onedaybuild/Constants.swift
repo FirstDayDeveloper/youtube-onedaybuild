1b5ca7d0e808ac81847b19f10d54e7366d2eb907
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-09-20 17:37:40 +0300

    Mocked network call and created Video Models

---
+++ youtube-onedaybuild/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild
+//
+//  Created by Kevin Nyangena on 9/20/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var API_KEY = "AIzaSyAtJXhqzPZvbfU_2TAMZWY0mcHhOTh5cj0"
+    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Kevin Nyangena on 9/20/22.
+//
+
+import Foundation
+
+struct video {
+    
+    var videoID = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
