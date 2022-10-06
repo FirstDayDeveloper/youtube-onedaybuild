e6382363b866bc2e33301f0ecadc8b54391a1882
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-10-06 13:58:39 +0300

    JSON decoding and parsing

--- youtube-onedaybuild/Video.swift
+++ youtube-onedaybuild/Video.swift
@@ -7,11 +7,56 @@
 
 import Foundation
 
-struct video {
+struct Video: Decodable {
     
     var videoID = ""
     var title = ""
     var description = ""
     var thumbnail = ""
-    var published = ""
+    var published = Date()
+    
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case snippet
+        case thumbnails
+        case high
+        case resourceId
+        
+        case published = "publishedAt"
+        case title
+        case description
+        case thumbnail = "url"
+        case videoID
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let snippetContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .snippet)
+        
+        // Parse title
+        self.title = try snippetContainer.decode(String.self, forKey: .title)
+        
+        // Parse description
+        self.description = try snippetContainer.decode(String.self, forKey: .description)
+        
+        // Parse publish date
+        self.published = try snippetContainer.decode(Date.self, forKey: .published)
+        
+        // Parse thumbnails by extracting from relevant containers
+        let thumbnailContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .thumbnails)
+            
+            // Extracting the high quality thumbnail from the thumbnail container
+            let highContainer = try thumbnailContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .high)
+            // Parsing thumbnails
+            self.thumbnail = try highContainer.decode(String.self, forKey: .thumbnail)
+        
+        // Parse Video ID
+        let resourceIdContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .resourceId)
+        
+        self.videoID = try resourceIdContainer.decode(String.self, forKey: .videoID)
+        
+    }
 }
--- youtube-onedaybuild/VideoModel.swift
+++ youtube-onedaybuild/VideoModel.swift
@@ -22,19 +22,29 @@ class Model {
         let session = URLSession.shared
         
         // Get a data task from the URLSession object
-        let dataTask = session.dataTask(with: url!) { data, response, error in
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
             
             // Check if there were any errors
             if error != nil || data == nil {
                 return
             }
+            
+            do {
             // Parsing the data into video objects
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            let response = try decoder.decode(Response.self, from: data!)
+                
+                dump(response)
         
+            }
+            catch {
+            
+            }
         }
         
         // Kick off the task
         dataTask.resume()
-        
-        
     }
 }
